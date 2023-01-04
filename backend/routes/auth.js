87b7269daeab3a6332638e98c2ce70095dbce45d
Abdullah <ksyan321@gmail.com>
2023-01-04 15:25:02 +0500

    tut 6

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET =
+  "thisisahighlyconfidentailsecretwhichyouwillneverbeabletolook('.')at";
 
 // Create a user using: POST "/api/auth/createuser". No login required
 router.post(
@@ -25,14 +30,24 @@ router.post(
           .status(400)
           .json({ errors: "Sorry a User with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       // Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.json(req.body);
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authtoken });
+      // res.json(user);
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Some Error occured");
