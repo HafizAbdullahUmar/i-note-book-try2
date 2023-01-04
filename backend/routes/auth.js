98017336d7af7bb138849e5c5ad02a2b1e56dc66
Abdullah <ksyan321@gmail.com>
2023-01-04 11:30:56 +0500

    tut 4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "test",
-    age: 18,
-  };
-
-  res.json(obj);
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 module.exports = router;
